7da2073109b49306ccc138f511db1a2d8a2c34bb
mryhryki <mryhryki@gmail.com>
2024-03-27 20:40:39 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
 Deno.serve(async (request): Promise<Response> => {
-  return new Response("Hello, world!");
+  const path = request.url.endsWith("/") ? `${request.url}index.html` : request.url;
+
+  const html = await Deno.readFile(`./static${path}`).catch(() => null);
+  if (html != null) {
+    return new Response(html, {
+      headers: new Headers({
+        "Content-Type": "text/html; charset=utf-8",
+      }),
+    });
+  }
+
+  return new Response("Not Found", {
+    status: 404,
+    headers: new Headers({
+      "Content-Type": "text/plain; charset=utf-8",
+    }),
+  });
 });
