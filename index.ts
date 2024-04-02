46cd16df7c8f02a8bed5a66a2d775d0b6df6c69b
mryhryki <mryhryki@gmail.com>
2024-04-02 19:22:42 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
+const keyPair = await crypto.subtle.generateKey({
+  name: "ECDSA",
+  namedCurve: "P-256",
+}, false, ["sign", "verify"]);
+
 Deno.serve(async (request): Promise<Response> => {
   const {pathname} = new URL(request.url)
   const filePath = pathname.endsWith("/") ? `./static${pathname}index.html` : `./static${pathname}`;
 
-  const html = await Deno.readFile(filePath).catch(() => null);
+  const html: Uint8Array | null = await Deno.readFile(filePath).catch(() => null);
   if (html != null) {
-    return new Response(html, {
+    const publicKey = await crypto.subtle.exportKey("raw", keyPair.publicKey)
+    const htmlString = new TextDecoder().decode(html).replace("'[[PUBLIC-KEY]]'", JSON.stringify(Array.from(new Uint8Array(publicKey))));
+    return new Response(htmlString, {
       status: 200,
       headers: new Headers({
         "Content-Type": getMimeType(filePath),
