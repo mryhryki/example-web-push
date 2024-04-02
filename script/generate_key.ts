b56819b2e50021d11d328f8eb04c85a69832892c
mryhryki <mryhryki@gmail.com>
2024-04-02 19:15:16 +0900

    Add script/generate_key.ts

---
+++ script/generate_key.ts
@@ -0,0 +1,13 @@
+import { encodeBase64 } from "https://deno.land/std@0.221.0/encoding/base64.ts";
+
+const keyPair = await crypto.subtle.generateKey({
+  name: "ECDSA",
+  namedCurve: "P-256",
+}, true, ["sign", "verify"]);
+
+const privateKey = await crypto.subtle.exportKey("pkcs8", keyPair.privateKey);
+await Deno.writeTextFile("./temp/private.key", encodeBase64(privateKey));
+
+const publicKey = await crypto.subtle.exportKey("raw", keyPair.publicKey);
+await Deno.writeTextFile("./temp/public.key", encodeBase64(publicKey));
+
