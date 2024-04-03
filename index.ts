67f301cf15221c129a56852681ea4822947a1522
mryhryki <mryhryki@gmail.com>
2024-04-03 10:57:44 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,18 +1,34 @@
 import { encodeBase64 } from "https://deno.land/std@0.221.0/encoding/base64.ts";
 
-const keyPair = await crypto.subtle.generateKey({
-  name: "ECDSA",
-  namedCurve: "P-256",
-}, false, ["sign", "verify"]);
+const keyPair = await crypto.subtle.generateKey(
+  {
+    name: "ECDSA",
+    namedCurve: "P-256",
+  },
+  false,
+  ["sign", "verify"],
+);
 
 Deno.serve(async (request): Promise<Response> => {
-  const {pathname} = new URL(request.url)
-  const filePath = pathname.endsWith("/") ? `./static${pathname}index.html` : `./static${pathname}`;
+  const { pathname } = new URL(request.url);
+  const filePath = pathname.endsWith("/")
+    ? `./static${pathname}index.html`
+    : `./static${pathname}`;
 
-  const html: Uint8Array | null = await Deno.readFile(filePath).catch(() => null);
+  const html: Uint8Array | null = await Deno.readFile(filePath).catch(() =>
+    null
+  );
   if (html != null) {
-    const publicKey = await crypto.subtle.exportKey("raw", keyPair.publicKey)
-    const htmlString = new TextDecoder().decode(html).replace("'[[PUBLIC-KEY]]'", JSON.stringify(encodeBase64(new Uint8Array(publicKey))));
+    const publicKey = await crypto.subtle.exportKey("raw", keyPair.publicKey);
+    const htmlString = new TextDecoder().decode(html).replace(
+      "'[[PUBLIC-KEY]]'",
+      JSON.stringify(
+        encodeBase64(new Uint8Array(publicKey))
+          .replace(/\+/g, "-")
+          .replace(/\//g, "_")
+          .replace(/=/g, ""),
+      ),
+    );
     return new Response(htmlString, {
       status: 200,
       headers: new Headers({
@@ -36,4 +52,4 @@ const getMimeType = (filePath: string): string => {
     return "text/javascript";
   }
   return "text/plain; charset=utf-8";
-}
+};
