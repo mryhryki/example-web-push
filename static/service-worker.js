586c58b07eff9ed8991a70031289c5774f62cb86
mryhryki <mryhryki@gmail.com>
2024-04-03 18:06:54 +0900

    Update static/service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener("push", async (event) => {
+  // const payload = event.data.json();
+  // console.debug("Payload:", payload)
+  await self.registration.showNotification("TEST", {
+    body: 'description'
+  });
+})
