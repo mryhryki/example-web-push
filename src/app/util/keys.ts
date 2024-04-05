0723508811779b5fc947a608644e29f441e94ec0
mryhryki <mryhryki@gmail.com>
2024-04-05 18:11:38 +0900

    Add src/app/util/keys.ts

---
+++ src/app/util/keys.ts
@@ -0,0 +1,5 @@
+const AppPublicKey = "BGVrcQSsXsKIz5qO5VSLkWwPfbAXDaIzUtt-d7wjBvwH4XI7a4mNzxRQarGd_08HokyNuOrGD7Ro3WDHhWyv0Uw";
+const AppPrivateKey = "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgkl_nmFQE3v05rx8xOweENkKCzUO-qcDuuNg-tiw1CWKhRANCAARla3EErF7CiM-ajuVUi5FsD32wFw2iM1Lbfne8Iwb8B-FyO2uJjc8UUGqxnf9PB6JMjbjqxg-0aN1gx4Vsr9FM";
+
+export const getAppPublicKey = () => `${AppPublicKey}`;
+export const getAppPrivateKey = () => `${AppPrivateKey}`;
