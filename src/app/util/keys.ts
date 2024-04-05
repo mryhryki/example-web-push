6ce2bd747d31ac033eaecf3859bb3c997d2fe17c
mryhryki <mryhryki@gmail.com>
2024-04-05 19:26:18 +0900

    Update src/app/util/keys.ts

--- src/app/util/keys.ts
+++ src/app/util/keys.ts
@@ -1,2 +1,10 @@
-export const getAppPrivateKey = () => "op3carwIWrhN2OuCW41J733z7AI_stvtTm2fKhp4loo";
-export const getAppPublicKey = () => "BIkz7_1ZnX48cQGrQK1mwzkuxxoDUW9id2aThnT6LuSgCaPtadLjVvoHGBna4zRQARXSUx3Z2q2Q8R27e6vksCY";
+const getRequiredValue = (key: string) => {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing required environment variable ${key}`);
+  }
+  return value;
+};
+
+export const getServerPrivateKey = () => getRequiredValue("SERVER_PRIVATE_KEY");
+export const getServerPublicKey = () => getRequiredValue("SERVER_PUBLIC_KEY");
