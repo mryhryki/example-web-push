6aefbda6760553aec9be633dbd4e067cec2331a0
mryhryki <mryhryki@gmail.com>
2024-04-05 18:54:43 +0900

    Update src/app/util/keys.ts

--- src/app/util/keys.ts
+++ src/app/util/keys.ts
@@ -1,2 +1,2 @@
-export const getAppPrivateKey = () => "LVNTSU9idkxmV1hWSDl2bm4tY056N28xXzc5NEpNNGpFaWhmR1ltdnp0Yw";
-export const getAppPublicKey = () => "QktwRW5yN29ncGg2REE2eEhUOWdpVGNRaWNiYlhtR1ZiZVFpb29ta2NSUmhJZzRyRHk1QTZaVTRvY3BKd3BmMUF4N2J0UkMzNkgtVmY5TzhORzBGaVE0";
+export const getAppPrivateKey = () => "op3carwIWrhN2OuCW41J733z7AI_stvtTm2fKhp4loo";
+export const getAppPublicKey = () => "BIkz7_1ZnX48cQGrQK1mwzkuxxoDUW9id2aThnT6LuSgCaPtadLjVvoHGBna4zRQARXSUx3Z2q2Q8R27e6vksCY";
