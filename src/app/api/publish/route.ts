51e1831f8bb7a25ffb75b888411da04c47bcae77
mryhryki <mryhryki@gmail.com>
2024-04-05 18:59:34 +0900

    Update src/app/api/publish/route.ts

--- src/app/api/publish/route.ts
+++ src/app/api/publish/route.ts
@@ -1,28 +1,19 @@
-import { Subscription } from "@/app/hooks/useSubscribe";
 import { getAppPrivateKey, getAppPublicKey } from "@/app/util/keys";
-import type { NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 import webpush from "web-push";
 
-type ResponseData = {
-  message: string
-}
+webpush.setVapidDetails(
+  "mailto:mryhryki@gmail.com",
+  getAppPublicKey(),
+  getAppPrivateKey(),
+);
 
-export async function POST(
-  req: NextRequest,
-  res: NextApiResponse<ResponseData>,
-) {
-  const { endpoint, auth, p256dh } = await req.json() as Subscription;
-
-  webpush.setVapidDetails(
-    "mailto:mryhryki@gmail.com",
-    getAppPublicKey(),
-    getAppPrivateKey(),
-  );
+export async function POST(req: NextRequest) {
+  const { title, body, endpoint, auth, p256dh } = await req.json();
 
   const message = {
-    title: "Your Push Payload Title",
-    body: "Your Push Payload Body",
+    title: title ?? "(No title provided)",
+    body: body ?? "(No body provided)",
   };
 
   await webpush.sendNotification({
