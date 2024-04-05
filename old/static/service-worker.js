c3758bd0ff8ae96f76deaa36e54d6ee61f1d205d
mryhryki <mryhryki@gmail.com>
2024-04-05 09:21:37 +0900

    Remove old/ directory

--- old/static/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("push", async (event) => {
-  console.debug("PushEvent:", event)
-  const payload = event.data?.json();
-  console.debug("Payload:", payload)
-  await self.registration.showNotification("TEST", {
-    body: 'description'
-  });
-})
