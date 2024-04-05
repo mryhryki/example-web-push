9c286fe3b34ae73be4a57465ee19eaea9503ca6a
mryhryki <mryhryki@gmail.com>
2024-04-05 18:43:23 +0900

    Update src/app/hooks/useSubscribe.ts

--- src/app/hooks/useSubscribe.ts
+++ src/app/hooks/useSubscribe.ts
@@ -1,10 +1,9 @@
 import { getAppPublicKey } from "@/app/util/keys";
 import { useCallback, useState } from "react";
-
-interface Subscription {
+export interface Subscription {
   endpoint: string;
-  auth: string; // Base64
-  p256dh: string; // Base64
+  auth: string; // Base64url
+  p256dh: string; // Base64url
 }
 
 interface SubscribeState {
@@ -12,25 +11,20 @@ interface SubscribeState {
   subscription: Subscription | null;
 }
 
-export const useSubscribe = (registration: ServiceWorkerRegistration | null): SubscribeState => {
+export const useSubscribe = (publicKey: string,registration: ServiceWorkerRegistration | null): SubscribeState => {
   const [subscription, setSubscription] = useState<Subscription | null>(null);
 
   const subscribe = useCallback(async () => {
     if (registration == null) return;
 
-    const publicKey = getAppPublicKey();
     const subscription = await registration.pushManager.subscribe({
       userVisibleOnly: true,
       applicationServerKey: publicKey,
     });
 
-    console.debug("Subscription:", subscription);
     // https://web.dev/articles/push-notifications-web-push-protocol?hl=ja#inputs
     const auth = subscription.getKey("auth");
     const p256dh = subscription.getKey("p256dh");
-
-    console.log("Subscription.auth:", auth);
-    console.log("Subscription.p256dh:", p256dh);
 
     setSubscription({
       endpoint: subscription.endpoint,
