7e87fc07da79572602dc56a4503f8ca649fb42bc
mryhryki <mryhryki@gmail.com>
2024-04-05 18:11:38 +0900

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { useServiceWorker } from "@/app/hooks/useServiceWorker";
+import { useSubscribe } from "@/app/hooks/useSubscribe";
+
 export default function Home() {
+  const registration = useServiceWorker();
+  const { subscribe, subscription } = useSubscribe(registration);
+
   return (
     <main>
       <h1>
@@ -10,6 +18,47 @@ export default function Home() {
           mryhryki/example-web-push
         </a>
       </h1>
+
+      <section>
+        <h2>Status</h2>
+        <ul>
+          <li>Service Worker: {registration != null ? "Registered" : "Not registered"}</li>
+          <li>Subscribe: {subscription != null ? "Subscribed" : "Not subscribed"} </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Subscribe</h2>
+        <p>
+          <button
+            onClick={subscribe}
+            style={{ padding: "0.5em 1em" }}
+            disabled={registration == null}
+          >
+            Subscribe
+          </button>
+        </p>
+        {subscription != null && (
+          <ul>
+            <li>Endpoint: <code>{subscription.endpoint}</code></li>
+            <li>auth: <code>{subscription.auth}</code></li>
+            <li>p256dh: <code>{subscription.p256dh}</code></li>
+          </ul>
+        )}
+      </section>
+
+      <section>
+        <h2>Subscribe</h2>
+        <p>
+          <button
+            /* TODO: Publish */
+            style={{ padding: "0.5em 1em" }}
+            disabled={subscription == null}
+          >
+            Publish
+          </button>
+        </p>
+      </section>
     </main>
   );
 }
