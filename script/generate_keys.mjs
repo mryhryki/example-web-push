6dccf283c577a47de306305cfcbcbd6e9cf161f2
mryhryki <mryhryki@gmail.com>
2024-04-05 18:51:57 +0900

    Update script/generate_keys.mjs

--- script/generate_keys.mjs
+++ script/generate_keys.mjs
@@ -1,7 +1,5 @@
 import webpush from "web-push";
 
-const vapidKeys = webpush.generateVAPIDKeys();
-
 const main = async () => {
   // const keyPair = await crypto.subtle.generateKey(
   //   {
@@ -13,10 +11,12 @@ const main = async () => {
   // );
 
   // const privateKey = await crypto.subtle.exportKey("pkcs8", keyPair.privateKey);
-  const base64EncodedPrivateKey = Buffer.from(vapidKeys.privateKey).toString("base64url");
-  console.log("[Application Private Key (PKCS#8)]\n%s\n", base64EncodedPrivateKey);
-
   // const publicKey = await crypto.subtle.exportKey("raw", keyPair.publicKey);
+
+  const vapidKeys = webpush.generateVAPIDKeys();
+  const base64EncodedPrivateKey = Buffer.from(vapidKeys.privateKey).toString("base64url");
+  console.log("[Application Private Key]\n%s\n", base64EncodedPrivateKey);
+
   const base64EncodedPublicKey = Buffer.from(vapidKeys.publicKey).toString("base64url");
   console.log("[Application Public Key]\n%s\n", base64EncodedPublicKey);
 }
