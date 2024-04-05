ab35c55f6a26c2d7000fdfd4928869c7d64537e5
mryhryki <mryhryki@gmail.com>
2024-04-05 18:11:37 +0900

    Add script/generate_keys.js

---
+++ script/generate_keys.js
@@ -0,0 +1,20 @@
+const main = async () => {
+  const keyPair = await crypto.subtle.generateKey(
+    {
+      name: "ECDSA",
+      namedCurve: "P-256",
+    },
+    true,
+    ["sign", "verify"],
+  );
+
+  const privateKey = await crypto.subtle.exportKey("pkcs8", keyPair.privateKey);
+  const base64EncodedPrivateKey = Buffer.from(privateKey).toString("base64url");
+  console.log("[Application Private Key (PKCS#8)]\n%s\n", base64EncodedPrivateKey);
+
+  const publicKey = await crypto.subtle.exportKey("raw", keyPair.publicKey);
+  const base64EncodedPublicKey = Buffer.from(publicKey).toString("base64url");
+  console.log("[Application Public Key]\n%s\n", base64EncodedPublicKey);
+}
+
+main()
