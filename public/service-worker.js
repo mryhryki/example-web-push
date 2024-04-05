88f46d99b57327d6e588347ab6abf915bcb68d0a
mryhryki <mryhryki@gmail.com>
2024-04-05 18:57:20 +0900

    Update public/service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,8 @@ self.addEventListener("push", async (event) => {
   console.debug("PushEvent:", event)
   const payload = event.data?.json();
   console.debug("Payload:", payload)
-  await self.registration.showNotification("TEST", {
-    body: 'description'
-  });
+  await self.registration.showNotification(
+    payload.title,
+    { body: payload.body }
+  );
 })
